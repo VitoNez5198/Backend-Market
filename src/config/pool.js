5c6dc8e684f2e1c68bc563e424776892e8572b4c
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-08-10 13:31:07 -0400

    estable

--- src/config/pool.js
+++ src/config/pool.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 
+const connectionString = "postgresql://postgres:ClcjRgsSTeEw1QXNigK1@containers-us-west-208.railway.app:5538/railway";
+
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     allowExitOnIdle: true
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    allowExitOnIdle: true
+    connectionString,
 });
 
 module.exports = pool;
