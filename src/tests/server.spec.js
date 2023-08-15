d939000a3d73a0e70041ea0569bbd0f525498fe5
VitoNez5198 <113071024+VitoNez5198@users.noreply.github.com>
2023-08-14 21:49:00 -0400

    test actualizados

--- src/tests/server.spec.js
+++ src/tests/server.spec.js
@@ -67,3 +67,54 @@ describe("Operaciones CRUD de Auth", () => {
         });
     });
 });
+
+describe("Operaciones CRUD de Favorites", () => {
+    describe("Obteniendo resultados de GET /favorites", () => {
+        it("Devolviendo un status 200", async () => {
+            const response = await request(server).get('/favorites').send();
+            const status = response.statusCode;
+            expect(status).toBe(200);
+        });
+    });
+});
+
+describe('Operaciones CRUD de Artworks', () => {
+    describe('Obteniendo resultados de GET /artworks', () => {
+        it('Debería devolver un status 200 y obtener todos los artworks', async () => {
+            const response = await request(server)
+                .get('/artworks');
+
+            expect(response.status).toBe(200);
+            expect(Array.isArray(response.body)).toBe(true);
+        });
+    });
+
+    describe('Actualizando un artwork existente', () => {
+        it('Debería devolver un status 200 y actualizar un artwork existente', async () => {
+            const updatedArtwork = {
+                product_id: 2,
+                title: 'Nuevo Artwork',
+                description: 'Descripción del nuevo artwork',
+                price: 200,
+                url_image: 'https://upload.wikimedia.org/wikipedia/commons/4/43/Pair_of_mandarin_ducks.jpg',
+                seller_id: 4,
+            };
+
+            const { body: artwork, status } = await request(server)
+                .put('/artworks/2') // Reemplaza el nuemro con el ID del artwork a actualizar
+                .send(updatedArtwork);
+
+            expect(status).toBe(200);
+            expect(artwork).toMatchObject(updatedArtwork);
+        });
+    });
+
+    describe('Eliminando un artwork existente', () => {
+        it('Debería devolver un status 200 al eliminar un artwork existente', async () => {
+            const response = await request(server)
+                .delete('/artworks/6'); // Reemplaza el numero con el ID del artwork a eliminar
+        
+            expect(response.status).toBe(200);
+        });
+    });
+});
